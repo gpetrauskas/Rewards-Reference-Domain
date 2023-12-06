b4de6d4e098eb1753a35ed4c0680b79845396d63
gpetrauskas <inconoir@gmail.com>
2023-12-06 20:22:30 +0200

    Configure Spring beans in RewardsConfig
    - Define @Bean methods for rewardNetwork, accountRepository,
      restaurantRepository, and rewardRepository.
    - Inject DataSource through constructor injection.
    - Implement each @Bean method to instantiate its object and
      set its dependencies (JdbcAccountRepository,
      JdbcRestaurantRepository, and JdbcRewardRepository).

--- lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -1,5 +1,17 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
+
 import javax.sql.DataSource;
 
 /**
@@ -42,9 +54,52 @@ import javax.sql.DataSource;
  *   not an implementation.
  */
 
+// TODO-01 make this class a spring configuration class
+@Configuration
 public class RewardsConfig {
 
 	// Set this by adding a constructor.
 	private DataSource dataSource;
+
+	// TODO-03 Inject DataSource through constructor injection
+	// Autowired is optional as it is only constructor
+	@Autowired
+	public RewardsConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	// TODO-02 Defining empty bean methods
+	// TODO-02 - rewardNetwork
+	@Bean
+	public RewardNetwork rewardNetwork() {
+		return new RewardNetworkImpl(accountRepository(), restaurantRepository(), rewardRepository());
+	}
+
+	// TODO-02 - accountRepository
+	@Bean
+	public AccountRepository accountRepository() {
+		// TODO-04 instantiate JdbcAccountRepository
+		JdbcAccountRepository jdbcAccountRepository = new JdbcAccountRepository();
+		jdbcAccountRepository.setDataSource(dataSource);
+		return jdbcAccountRepository;
+	}
+
+	// TODO-02 - restaurantRepository
+	@Bean
+	public RestaurantRepository restaurantRepository() {
+		// TODO-04 Instantiate JdbcRestaurantRepository
+		JdbcRestaurantRepository jdbcRestaurantRepository = new JdbcRestaurantRepository();
+		jdbcRestaurantRepository.setDataSource(dataSource);
+		return jdbcRestaurantRepository;
+	}
+
+	// TODO-02 - rewardRepository
+	@Bean
+	public RewardRepository rewardRepository() {
+		// TODO-04 instantiate JdbcRewardRepository
+		JdbcRewardRepository jdbcRewardRepository = new JdbcRewardRepository();
+		jdbcRewardRepository.setDataSource(dataSource);
+		return jdbcRewardRepository;
+	}
 
 }
