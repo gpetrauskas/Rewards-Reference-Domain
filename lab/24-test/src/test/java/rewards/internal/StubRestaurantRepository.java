5ac80033368d4fb5032595a8401230265d65a9c4
gpetrauskas <inconoir@gmail.com>
2023-12-14 00:17:38 +0200

    Assign profiles to repository classes

--- lab/24-test/src/test/java/rewards/internal/StubRestaurantRepository.java
+++ lab/24-test/src/test/java/rewards/internal/StubRestaurantRepository.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import org.springframework.stereotype.Repository;
@@ -24,6 +25,7 @@ import common.money.Percentage;
  * behavior by considering the state of this stub.
  */
 @Repository
+@Profile("stub")
 public class StubRestaurantRepository implements RestaurantRepository {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
