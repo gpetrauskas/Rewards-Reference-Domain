04fb2a935184a520cc19a7533cdf2cb6fd1ef61d
gpetrauskas <inconoir@gmail.com>
2023-12-13 23:50:34 +0200

    Added @Repository annotation

--- lab/24-test/src/test/java/rewards/internal/StubRewardRepository.java
+++ lab/24-test/src/test/java/rewards/internal/StubRewardRepository.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.stereotype.Repository;
 import rewards.AccountContribution;
 import rewards.Dining;
 import rewards.RewardConfirmation;
@@ -13,6 +14,7 @@ import rewards.internal.reward.RewardRepository;
 /**
  * A dummy reward repository implementation.
  */
+@Repository
 public class StubRewardRepository implements RewardRepository {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
