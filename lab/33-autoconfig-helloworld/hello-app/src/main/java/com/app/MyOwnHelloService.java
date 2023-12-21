80772f86d6015570db4a45d29447f3e9e528c6f0
gpetrauskas <inconoir@gmail.com>
2023-12-21 13:49:29 +0200

    Created MyOwnHelloService class

--- lab/33-autoconfig-helloworld/hello-app/src/main/java/com/app/MyOwnHelloService.java
+++ lab/33-autoconfig-helloworld/hello-app/src/main/java/com/app/MyOwnHelloService.java
@@ -1,2 +1,11 @@
-package com.app;public class MyOwnHelloService {
+package com.app;
+
+import com.lib.HelloService;
+import org.springframework.context.annotation.Bean;
+
+public class MyOwnHelloService implements HelloService {
+    @Bean
+    public void greet() {
+        System.out.println("Hello, myOwnHelloService");
+    }
 }
