91a4093e20cd1ee8f879008abe12cd7fff730557
gpetrauskas <inconoir@gmail.com>
2023-12-28 10:49:48 +0200

    Created accountsDetails() method
    Defined URL mapping /accounts/{entityId}
    
    PathVariable annotation bind method parameter to the value of variable {entityId} in the URL
    
    Return account information

--- lab/36-mvc/src/main/java/accounts/web/AccountController.java
+++ lab/36-mvc/src/main/java/accounts/web/AccountController.java
@@ -2,8 +2,11 @@ package accounts.web;
 
 import accounts.AccountManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriTemplate;
 import rewards.internal.account.Account;
 
 import java.util.List;
@@ -59,6 +62,11 @@ public class AccountController {
 	//   needed to retrieve an account.
 	// - Use the accountManager to obtain an account. This is the value to return
 	// - Save all work.
+	@GetMapping("/accounts/{entityId}")
+	public ResponseEntity<Account> accountDetails(@PathVariable Long entityId) {
+		
+		return ResponseEntity.ok(accountManager.getAccount(entityId));
+	}
 
 
 	// TODO-10b: If AccountControllerTests.testHandleDetailsRequest()
